b3bd1b0e988c9a587cce3729834a41624f2b9c34
Damian Niewiadomski <damian@niewiadomski.net>
2023-11-02 18:18:11 +0100

    Adds huego

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module hue
 
 go 1.21.3
+
+require github.com/amimof/huego v1.2.1
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/amimof/huego"
+)
+
+func main() {
+	bridge := huego.New("192.168.1.59", "username")
+	l, err := bridge.GetLights()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Found %d lights", len(l))
 }
