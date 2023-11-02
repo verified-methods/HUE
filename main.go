ded82f4bbab99d23a04f4f7dee8d71edc1a2b74e
Damian Niewiadomski <damian@niewiadomski.net>
2023-11-02 19:39:30 +0100

    Adds cleanenv

--- main.go
+++ main.go
@@ -2,15 +2,49 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/amimof/huego"
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
+type ConfigHue struct {
+	Host  string `yaml:"host"  env:"HOST"`
+	Token string `yaml:"token" env:"TOKEN"`
+}
+
+var cfg ConfigHue
+
 func main() {
-	bridge := huego.New("192.168.1.59", "username")
-	l, err := bridge.GetLights()
+	err := cleanenv.ReadConfig("config.yml", &cfg)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	fmt.Printf("Found %d lights", len(l))
+	bridge := huego.New(cfg.Host, cfg.Token)
+	light, err := bridge.GetLight(1)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+
+	// Check the current state of the light
+	state := light.State
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+
+	// If the light is on, turn it off; if it's off, turn it on
+	if state.On {
+		_ = light.Off()
+		if err != nil {
+			fmt.Printf("Error: %s", err)
+		}
+		fmt.Println("The light was on, it's now off")
+	} else {
+		_ = light.On()
+		if err != nil {
+			fmt.Printf("Error: %s", err)
+		}
+		fmt.Println("The light was off, it's now on")
+	}
+
 }
